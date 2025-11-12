67ef332128f627e50207e4f118edb33eec87b3f1
Angeles <agervacio2022@gmail.com>
2025-11-11 18:13:10 -0600

    Save current AFL++ fuzzing progress

--- bugtest.c
+++ bugtest.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    char buf[4];
-    if (argc < 2) return 0;
-    strcpy(buf, argv[1]);  // intentionally unsafe for fuzzing
-    if (buf[0] == 'X') printf("Crash!\n");
+int main(int argc, char* argv[]) {
+    char buf[8];
+
+    if (argc < 2) {
+        printf("No input provided.\n");
+        return 0;
+    }
+
+    // Check length before copying (so not all inputs crash)
+    if (strlen(argv[1]) < sizeof(buf)) {
+        strcpy(buf, argv[1]);
+        printf("Input: %s\n", buf);
+    } else {
+        printf("Too long input detected!\n");
+    }
+
     return 0;
 }
+
+
